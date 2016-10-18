f8ad40b7ce5595c5cca6345ba88e6f3ce1769742
Dave Moten <davidmoten@gmail.com>
2016-10-18 18:11:08 +1100

    refactor

--- src/main/java/com/github/davidmoten/clave/Entry.java
+++ src/main/java/com/github/davidmoten/clave/Entry.java
@@ -6,11 +6,12 @@ import java.util.stream.Collectors;
 
 import javax.xml.bind.annotation.XmlAccessOrder;
 import javax.xml.bind.annotation.XmlAccessorOrder;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import com.github.davidmoten.clave.Entry.EntryAdapter;
-import com.github.davidmoten.clave.EntryMutable.AttachmentMetadataMutable;
+import com.github.davidmoten.clave.Entry.EntryMutable.AttachmentMetadataMutable;
 
 @XmlJavaTypeAdapter(EntryAdapter.class)
 @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
@@ -50,18 +51,17 @@ public class Entry {
         }
     }
 
-    public static class EntryAdapter extends XmlAdapter<EntryMutable, Entry> {
+    static class EntryAdapter extends XmlAdapter<EntryMutable, Entry> {
 
         @Override
         public Entry unmarshal(EntryMutable v) throws Exception {
-            return new Entry(v.getId(), v.getName(), v.getUsername(), v.getPasswordId(),
-                    v.getPasswordSalt(), v.getNotes(), v.getUrl(), v.getTags(),
-                    v.getAttachments().stream().map(a -> toAttachmentMutable(a))
+            return new Entry(v.id, v.name, v.username, v.passwordId, v.passwordSalt, v.notes, v.url,
+                    v.tags, v.attachments.stream().map(a -> toAttachmentMutable(a))
                             .collect(Collectors.toList()));
         }
 
         private static AttachmentMetadata toAttachmentMutable(AttachmentMetadataMutable a) {
-            return new AttachmentMetadata(a.getId(), a.getSize());
+            return new AttachmentMetadata(a.id, a.size);
         }
 
         @Override
@@ -73,6 +73,49 @@ public class Entry {
 
         private AttachmentMetadataMutable toAttachment(AttachmentMetadata a) {
             return new AttachmentMetadataMutable(a.id, a.size);
+        }
+
+    }
+
+    @XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
+    static class EntryMutable {
+
+        public String id;
+        public String name;
+        public String username;
+        // corresponds to the name of the ZipEntry containing the password bytes
+        public String passwordId;
+        public String passwordSalt;
+        public String notes;
+        public String url;
+        @XmlElement(name = "tag")
+        public Set<String> tags;
+        @XmlElement(name = "attachment")
+        public List<AttachmentMetadataMutable> attachments;
+
+        private EntryMutable(String id, String name, String username, String passwordId,
+                String passwordSalt, String notes, String url, Set<String> tags,
+                List<AttachmentMetadataMutable> attachments) {
+            this.id = id;
+            this.name = name;
+            this.username = username;
+            this.passwordId = passwordId;
+            this.passwordSalt = passwordSalt;
+            this.notes = notes;
+            this.url = url;
+            this.tags = tags;
+            this.attachments = attachments;
+        }
+
+        static final class AttachmentMetadataMutable {
+
+            public String id;
+            public int size;
+
+            AttachmentMetadataMutable(String id, int size) {
+                this.id = id;
+                this.size = size;
+            }
         }
 
     }
