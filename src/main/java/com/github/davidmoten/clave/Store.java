6127797bc1895c7c77cf2766ec0485dacd5eb012
Dave Moten <davidmoten@gmail.com>
2016-10-18 12:15:50 +1100

    dev

--- src/main/java/com/github/davidmoten/clave/Store.java
+++ src/main/java/com/github/davidmoten/clave/Store.java
@@ -9,57 +9,61 @@ import com.github.davidmoten.clave.Tokens.Info;
 
 public final class Store {
 
-	private static final Store instance = new Store();
+    private static final Store instance = new Store();
 
-	private static final int SALT_LENGTH = 20;
+    private static final int SALT_LENGTH = 20;
 
-	private final Map<String, HashAndSalt> values = new ConcurrentHashMap<String, HashAndSalt>();
-	private final CipherKey cipherKey = new CipherKey();
+    private final Map<String, HashAndSalt> values = new ConcurrentHashMap<String, HashAndSalt>();
+    private final CipherKey cipherKey = new CipherKey();
 
-	public static Store instance() {
-		return instance;
-	}
-	
-	public byte[] cipherKey() {
-		return cipherKey.value();
-	}
+    public static Store instance() {
+        return instance;
+    }
 
-	public void createAccount(String username, String password) {
-		if (values.containsKey(username)){
-			throw new UsernameExistsAlreadyException();
-		}
-		byte[] salt = createSalt();
-		byte[] hash = Pbkdf2.hashPassword(password, salt);
-		values.put(username, new HashAndSalt(hash, salt));
-	}
-	
-	public void changePassword(String username, String currentPassword, String newPassword) {
-		HashAndSalt hs = values.get(username);
-		if (hs == null) {
-			throw new WrongUsernameOrPasswordException();
-		} else {
-		    byte[] hash1 = Pbkdf2.hashPassword(currentPassword, hs.salt);
-		    if (!Arrays.equals(hash1, hs.hash)) {
-		    	throw new WrongUsernameOrPasswordException();
-		    } else {
-		    	byte[] salt = createSalt();
-				byte[] hash = Pbkdf2.hashPassword(newPassword, salt);
-				values.put(username, new HashAndSalt(hash, salt));
-		    }
-		}
-	}
+    public byte[] cipherKey() {
+        return cipherKey.value();
+    }
 
-	private  byte[] createSalt () {
-		SecureRandom random = new SecureRandom();
-		byte[] salt = new byte[SALT_LENGTH];
-		random.nextBytes(salt);
-		return salt;
-	}
+    public void createAccount(String username, String password) {
+        if (values.containsKey(username)) {
+            throw new UsernameExistsAlreadyException();
+        }
+        byte[] salt = createSalt();
+        byte[] hash = Pbkdf2.hashPassword(password, salt);
+        values.put(username, new HashAndSalt(hash, salt));
+    }
 
-	public String getPassword(String token, String key) {
-		Info info = Tokens.parseToken(token, cipherKey.value());
-		//TODO
-        return "";	
-	}
+    public void changePassword(String username, String currentPassword, String newPassword) {
+        HashAndSalt hs = values.get(username);
+        if (hs == null) {
+            throw new WrongUsernameOrPasswordException();
+        } else {
+            byte[] hash1 = Pbkdf2.hashPassword(currentPassword, hs.salt);
+            if (!Arrays.equals(hash1, hs.hash)) {
+                throw new WrongUsernameOrPasswordException();
+            } else {
+                byte[] salt = createSalt();
+                byte[] hash = Pbkdf2.hashPassword(newPassword, salt);
+                values.put(username, new HashAndSalt(hash, salt));
+            }
+        }
+    }
+
+    private byte[] createSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[SALT_LENGTH];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    public String getPassword(String token, String key, long timeoutMs, Clock clock) {
+        Info info = Tokens.parseToken(token, cipherKey.value());
+        if (info.createTime < clock.now() - timeoutMs) {
+            throw new SessionTimeoutException();
+        } else {
+            // TODO
+            return "boo";
+        }
+    }
 
 }
